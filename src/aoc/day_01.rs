ae54496bf4471864f5783831ae59ae1364428ac2
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-17 20:43:04 +0000

    feat: added day 1 part 2 solution

--- src/aoc/day_01.rs
+++ src/aoc/day_01.rs
@@ -1,7 +1,7 @@
 // super is equivalent to crate::aoc::utils;
 use super::utils;
 
-pub fn part1() {
+pub fn _part1() {
 
     let lines: Vec<String> = utils::read_input_whole("data/day_01/input.txt");
 
@@ -26,5 +26,40 @@ pub fn part1() {
     println!("Part 1: Elf number {} has the most calories at {}", max_elf.0, max_elf.1);
 
     assert_eq!(max_elf.1, 69501);
+
+}
+
+pub fn part1_part2() {
+
+    let lines: Vec<String> = utils::read_input_whole("data/day_01/input.txt");
+
+    let top_x: usize = 3;
+    let mut max_cals: Vec<usize> = vec![0; top_x];
+    let mut max_ids: Vec<usize> = vec![0; top_x];
+    let mut elf_cal_total: usize = 0;
+    let mut elf_id: usize = 1;
+
+    for line in lines{
+        if line != "" {
+            let calories: usize = line.trim().parse().unwrap();
+            elf_cal_total += calories;
+        } else {
+            let min_top_cals: &usize = max_cals.iter().min().unwrap();
+            let min_top_cals_idx: usize = max_cals.iter().position(|x: &usize| x == min_top_cals).unwrap();
+            if elf_cal_total > *min_top_cals {
+                max_cals[min_top_cals_idx] = elf_cal_total;
+                max_ids[min_top_cals_idx] = elf_id;
+            }
+            elf_id += 1;
+            elf_cal_total = 0;
+        }
+
+    }  
+
+    let top_x_total: usize = max_cals.iter().sum();
+
+    println!("Part 1: Elf number {} has the most calories at {}", max_ids[0], max_cals[0]);
+    println!("Part 2: Sum of top {} elves' calories is {}", top_x, top_x_total);
+
 
 }

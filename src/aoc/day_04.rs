1fd0a078fbb48edc33dd857b55e00a56eef7174b
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-21 19:08:10 +0000

    feat: added day 4 part 1 solution

--- src/aoc/day_04.rs
+++ src/aoc/day_04.rs
@@ -1,28 +1,58 @@
 // Advent of Code 2022 Day 4 Solutions
 // Source: https://adventofcode.com/2022/day/4
 
-use std::{fs::read_to_string, usize};
 use regex::Regex;
+use std::fs::read_to_string;
+use std::time::Instant;
 
-pub fn part_1 () {
-    let input: String = read_to_string("data/day_04/test_input.txt").unwrap();
+pub fn part_1() {
+    let timer: Instant = Instant::now();
 
-    let vec_input: Vec<Vec<usize>> = input
+    let input: String = read_to_string("data/day_04/input.txt").unwrap();
+
+    let num_overlaps: usize = input
         .lines()
-        .map(|x| parse_line(x.to_string()))
-        .collect();
+        .map(|x| parse_ints_in_line(&x))
+        .map(|y| overlapping_bounds(y))
+        .sum();
 
-    dbg!(&vec_input);
-
+    println!("Day 4 Part 1: Number of overlaps {:?}", num_overlaps);
+    println!("Elapsed time: {:.2?}", timer.elapsed());
 }
 
-/// parse input line into a vector of integers using regex
-fn parse_line(line: String) -> Vec<usize> {
-
+/// Parse input line into vector of integers using regex
+///
+/// # Arguments
+///
+/// * `line` - A string deonoting the line to parse
+fn parse_ints_in_line(line: &str) -> Vec<usize> {
     let pattern = Regex::new(r"[0-9]+").unwrap();
-
     pattern
-        .find_iter(line.as_str())
+        .find_iter(line)
         .map(|digits| digits.as_str().parse::<usize>().unwrap())
         .collect::<Vec<usize>>()
+}
+
+/// Check for overlapping bounds
+///
+/// # Arguments
+///
+/// * `limits` - A vector of limits, paired like [min1, max1, min2, max2]
+fn overlapping_bounds(limits: Vec<usize>) -> usize {
+    let diff1: usize = limits[1] - limits[0];
+    let diff2: usize = limits[3] - limits[2];
+
+    if diff1 >= diff2 {
+        if (limits[2] >= limits[0]) & (limits[3] <= limits[1]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        if (limits[0] >= limits[2]) & (limits[1] <= limits[3]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }

e295df55477ad4bfdf3358b27f7f288028428d39
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-21 17:41:04 +0000

    chore: day 4 setup

---
+++ src/aoc/day_04.rs
@@ -0,0 +1,11 @@
+// Advent of Code 2022 Day 4 Solutions
+// Source: https://adventofcode.com/2022/day/4
+
+use std::fs::read_to_string;
+
+pub fn part_1 () {
+    let input: String = read_to_string("data/day_04/test_input.txt").unwrap();
+    dbg!(input);
+
+    
+}

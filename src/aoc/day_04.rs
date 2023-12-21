4f9c465eac221b7e1c5d5f89c1c6b3e4a73a5f12
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-21 18:43:13 +0000

    feat: added day 4 regex line parsing

--- src/aoc/day_04.rs
+++ src/aoc/day_04.rs
@@ -1,11 +1,28 @@
 // Advent of Code 2022 Day 4 Solutions
 // Source: https://adventofcode.com/2022/day/4
 
-use std::fs::read_to_string;
+use std::{fs::read_to_string, usize};
+use regex::Regex;
 
 pub fn part_1 () {
     let input: String = read_to_string("data/day_04/test_input.txt").unwrap();
-    dbg!(input);
 
-    
+    let vec_input: Vec<Vec<usize>> = input
+        .lines()
+        .map(|x| parse_line(x.to_string()))
+        .collect();
+
+    dbg!(&vec_input);
+
+}
+
+/// parse input line into a vector of integers using regex
+fn parse_line(line: String) -> Vec<usize> {
+
+    let pattern = Regex::new(r"[0-9]+").unwrap();
+
+    pattern
+        .find_iter(line.as_str())
+        .map(|digits| digits.as_str().parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
 }

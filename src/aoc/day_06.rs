23a3fab435fc3ffc75cc835e6ab7f5c3ec594e99
Heuristic Pedals <heuristicpedals@gmail.com>
2023-12-24 20:32:13 +0000

    feat: added day 6 part 1 solution

--- src/aoc/day_06.rs
+++ src/aoc/day_06.rs
@@ -1,6 +1,31 @@
 // Advent of Code Day 6 Solutions
 // Source: https://adventofcode.com/2022/day/6
 
-pub fn part_1() {
-    println!("Day 6 part 1...");
+use std::fs::read_to_string;
+use itertools::Itertools;
+use std::time::Instant;
+
+pub fn part1() {
+
+    let timer: Instant = Instant::now();
+
+    let input: String = read_to_string("data/day_06/input.txt").unwrap();
+    let packet_window_size: usize = 4;
+
+    let packet_start: usize = find_marker(packet_window_size, &input);
+
+    println!("Day 6 Part 1: Packet start {}", packet_start);
+    println!("Elapsed time: {:.2?}", timer.elapsed());
+}
+
+pub fn find_marker(window_size: usize, input: &str) -> usize {
+    let (idx, _state) = input.chars()
+            .collect::<Vec<char>>()
+            .windows(window_size)
+            .enumerate()
+            .map(|(i, x)| (i, x.into_iter().unique().count() == window_size))
+            .find(|(_i, x)| *x == true)
+            .unwrap();
+
+    idx + window_size
 }
